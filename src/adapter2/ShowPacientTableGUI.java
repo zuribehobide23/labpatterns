3aa6767761cf7b33ece9a75f58f24856b663f852
iturrioz <iturrioz@192.168.1.51>
2023-10-30 12:24:25 +0100

    gui updated

--- src/adapter2/ShowPacientTableGUI.java
+++ src/adapter2/ShowPacientTableGUI.java
@@ -11,35 +11,27 @@ import domain.Covid19Pacient;
 
 public class ShowPacientTableGUI extends JFrame{
 	
+	JTable table;
+	Covid19Pacient pacient;
 	
-  public static void main(String[] args) {
-    
-    Covid19Pacient pacientAitor=new Covid19Pacient("aitor", 35);
-    pacientAitor.addSymptomByName("disnea", 2);
-    pacientAitor.addSymptomByName("cefalea", 1);
-
-    
-	JFrame frame=new ShowPacientTableGUI(pacientAitor);
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-    frame.pack();
-    frame.setVisible(true);
-  }
   
   public ShowPacientTableGUI(Covid19Pacient pacient ) {
 	  	this.setTitle("Covid Symptoms "+pacient.getName());
+	  	
+	  	this.pacient=pacient;
+	  	
 	  	setFonts();
 	    
-		TableModel tm=new PacientTableModel(pacient);
-	    JTable table = new JTable(tm);
+	  	TableModel tm=new Covid19PacientTableModelAdapter(pacient);
+		table = new JTable(tm);
 	    table.setRowHeight(36);
 	    JScrollPane pane = new JScrollPane(table);
 	    pane.setPreferredSize(
-	      new java.awt.Dimension(300, 100));
+	      new java.awt.Dimension(300, 200));
 	    this.getContentPane().add(pane);
 	    
 	  
   }
-
 
   private static void setFonts() {
     Font font = new Font("Dialog", Font.PLAIN, 18);
